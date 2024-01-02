4a8600de0a052f7e96765911638df3f5961e8725
ljh8450 <dzigh30@gmail.com>
2024-01-02 18:07:43 +0900

    Corrcet answer!
    Refer to model answer

--- Album.py
+++ Album.py
@@ -1,27 +1,10 @@
 def solution(genres, plays):
-    hash_map = {}
-    count_map = {}
     answer = []
-    for music in range(len(plays)):
-        if genres[music] not in hash_map:
-            hash_map[genres[music]] = [plays[music]]
-        else:
-            if len(hash_map[genres[music]]) == 1:
-                hash_map[genres[music]].append(plays[music])
-            elif len(hash_map[genres[music]]) == 2:
-                hash_map[genres[music]].remove(min(hash_map[genres[music]]))
-                hash_map[genres[music]].append(plays[music])
-    for gen in list(hash_map.keys()):
-        count_map[gen] = sum(hash_map[gen])
-    count_map = sorted(count_map.items(), reverse = True)
-
-    for hash_values in len(plays):
-        if hash_values > len(hash_map.values()):
-            return answer
-        for i in range(len(plays)):
-            if plays[i] == hash_map.values()[hash_values] and genres[i] == hash_map.keys[hash_values//2]:
-                answer.append(i)
-    
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
-print(solution(genres, plays))
+    d = {e: [] for e in set(genres)}
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append([e[1], e[2]])
+    genreSort = sorted(list(d.keys()), key = lambda x: sum(map(lambda y: y[0], d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g], key = lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp), 2)]
+    return answer
