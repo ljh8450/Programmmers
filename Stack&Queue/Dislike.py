1f453fd19141f97668a874a944a82031a3a70619
ljh8450 <dzigh30@gmail.com>
2024-01-03 16:26:12 +0900

    Organize
    Organize folder

---
+++ Stack&Queue/Dislike.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    temp = 0
+    for i in range(len(arr)):
+        if len(answer) == 0:
+            answer.append(arr[i])
+        elif answer[-1] != arr[i]:
+            answer.append(arr[i])
+    return answer
