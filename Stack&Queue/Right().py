9f89cb9cf6c8e86a2a80998848f3c57e522bd16f
ljh8450 <dzigh30@gmail.com>
2024-01-04 20:36:23 +0900

    Update Right().py
    Add some lines for exception situations. But, still not correct

--- Stack&Queue/Right().py
+++ Stack&Queue/Right().py
@@ -3,22 +3,15 @@ def solution(s):
     index = 0
     data = [e for e in s]
     if s[-1] == "(" or s[0] == ")" or "()" not in s:
-        answer = False
-        return answer
-    while len(s) > 0:
+        return False
+    while len(data) > 0:
         if data[index]+data[index+1] == "()":
+            data.pop(index)
             data.pop(index)
             index = 0
         else:
             index += 0
-    return answer
-
-        
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
-
-s = "()()"
-print(solution(s))
+            if data[-1] == "(" or data[0] == ")" or ("()" not in data and len(data)>0):
+                return False
+        print(data)
+    return True
