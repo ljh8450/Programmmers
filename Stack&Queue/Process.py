237e117b0f7e24affc8d466759cd1f091c1cc044
ljh8450 <dzigh30@gmail.com>
2024-01-07 03:12:54 +0900

    Corret!
    Code was refer to model code

--- Stack&Queue/Process.py
+++ Stack&Queue/Process.py
@@ -1,17 +1,11 @@
 def solution(priorities, location):
+    queue = [(i, p) for i, p in enumerate(priorities)]
     answer = 0
-    data = []
-    index = 0
-    for i in range(len(priorities)):
-        M = max(priorities)
-        for j in range(i, len(priorities)):
-            if M == priorities[j]:
-                data.append(j)
-                priorities.pop(j)
-    print(data)
-    return answer
-
-priorities = [2, 1, 3, 2]
-location = 2
-
-print(solution(priorities, location))
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
