6f542de689b4089a0ba916484c2d3242d3031bb4
Miniberry <dzigh30@gmail.com>
2024-02-01 22:32:48 +0900

    Correct
    This code's refered to model codes

---
+++ Greedy/TrainingCloth.py
@@ -0,0 +1,20 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.reverse()
+
+    for i in reserve[:]:
+        if i in lost:
+            reserve.remove(i)
+            lost.remove(i)
+    
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+    return n-len(lost)
+n = 5
+lost = [2, 4]
+reserve = [1, 3, 5]
+
+print(solution(n, lost, reserve))
